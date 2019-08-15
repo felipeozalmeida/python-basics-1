2974050fddb1e91d6da7257c244b2afb1b649dce
Felipe Almeida <felipeortalmeida@gmail.com>
2019-08-14 23:40:51 -0300

    Formatted files

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 numbers = []
 
 for i in range(3):
-    numbers.append(input('Digite o valor {}: '.format(i + 1)))
+    numbers.append(input("Digite o valor {}: ".format(i + 1)))
 
 for number in numbers:
-    print('Número {}: {}'.format(number, number))
+    print("Número {}: {}".format(number, number))
--- ex2.py
+++ ex2.py
@@ -1,11 +1,14 @@
 class Worker:
     name = ""
     salary = 0.0
-    def __init__(self, name = "John", salary = 1000):
+
+    def __init__(self, name="John", salary=1000):
         self.name = name
         self.salary = salary
+
     def raiseSalary(self):
         self.salary *= 1.10
+
 
 print("\n***Create Worker***")
 name = input("Insert name: ")
@@ -15,6 +18,6 @@ worker = Worker(name, salary)
 print("\n***Raising salary by 10%***")
 worker.raiseSalary()
 
-print('\n***Worker***')
-print('Name: {}'.format(worker.name))
-print('Salary: {}'.format(worker.salary))
+print("\n***Worker***")
+print("Name: {}".format(worker.name))
+print("Salary: {}".format(worker.salary))
