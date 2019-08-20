d2b271014d3f9f5c7d588249e54e6ac0a8f35f3d
Felipe Almeida <felipeortalmeida@gmail.com>
2019-08-19 22:54:15 -0300

    Solved 4th exercise

---
+++ ex4.py
@@ -0,0 +1,18 @@
+debt = float(input("Enter debt: "))
+
+number_of_installments = int(input("How many installments do you want? (1, 3 or 5): "))
+
+if (number_of_installments == 1):
+    total = debt
+elif (number_of_installments == 3):
+    total = debt * 1.10
+elif (number_of_installments == 5):
+    total = debt * 1.20
+else:
+    print("Invalid value!")
+    pass
+
+installment = total / number_of_installments
+print("\n***Result***")
+print("Total: ${}".format(total))
+print("Installment: ${}".format(installment))
