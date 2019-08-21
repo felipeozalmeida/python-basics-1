b9f27d9a57dd6510965da4ce8223ce27a2e0e20f
Felipe Almeida <felipeortalmeida@gmail.com>
2019-08-21 00:34:34 -0300

    Moved code outside loop

--- ex5.py
+++ ex5.py
@@ -6,16 +6,16 @@ average = 0
 for i in range(3):
     try:
         grades.append(float(input("Insert grade no. {}: ".format(i + 1))))
-        
-        for grade in grades:
-            average += grade
-
-        average /= len(grades)
-
-        status = "Approved" if average >= 8 else "Disapproved"
     except ValueError:
         print("Incorrect value!")
         exit()
+
+for grade in grades:
+    average += grade
+
+average /= len(grades)
+
+status = "Approved" if average >= 8 else "Disapproved"
 
 print("\n***Student info***")
 print("Name: {}".format(name))
