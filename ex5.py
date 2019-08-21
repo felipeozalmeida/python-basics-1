1b2343f6e7650aba221826a2d123a7f15f34f162
Felipe Almeida <felipeortalmeida@gmail.com>
2019-08-21 00:18:11 -0300

    Solved 5th exercise

---
+++ ex5.py
@@ -0,0 +1,23 @@
+print("\n***Create student***")
+name = input("Insert your name: ")
+grades = []
+average = 0
+
+for i in range(3):
+    try:
+        grades.append(float(input("Insert grade no. {}: ".format(i + 1))))
+        
+        for grade in grades:
+            average += grade
+
+        average /= len(grades)
+
+        status = "Approved" if average >= 8 else "Disapproved"
+    except ValueError:
+        print("Incorrect value!")
+        exit()
+
+print("\n***Student info***")
+print("Name: {}".format(name))
+print("Avg.: {}".format(average))
+print("Status: {}".format(status))
